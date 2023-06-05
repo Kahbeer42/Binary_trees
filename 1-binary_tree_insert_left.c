911a3ef75a78ce9c2a27127c284cdb6654de3a9c
RosemaryEfebe247 <efebeoghenekaro@gmail.com>
2023-06-05 03:05:40 +0000

    binary_tree

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,12 +9,13 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
 
-	if (new == NULL)
-		return (NULL);
 	if (parent == NULL)
 		return (NULL);
 
 	new = binary_tree_node(parent, value);
-	parent->left = new;
+	if (new == NULL)
+		return (NULL);
+	if (parent->left == NULL)
+		parent->left = new;
 	return (new);
 }
